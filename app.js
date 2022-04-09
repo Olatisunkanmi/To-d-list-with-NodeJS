b44ee35afa01aec34848e53512a78d3462d35c49
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-04-09 19:30:34 +0100

    Adding MVC to the node application

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express');
+// const express = require('express');
 const bodyParser = require('body-parser');
-
+const todoCtrl = require('./controllers/todoCtrl');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+
+// Controllers 
+todoCtrl(app)
+
+app.listen(3000);
+console.log('Listening to port 3000');
---
+++ controllers/todoCtrl.js
@@ -0,0 +1,21 @@
+// Manipulate data handles, rountes etc.,
+
+module.exports = (app) => {
+
+    // GET. this is for the url itself
+    // 
+    app.get('/todo', (req, res) => {
+
+    });
+
+    // When user posts a data  
+    // When a user adds an item to the list. 
+    app.post('/todo', (req, res) => {
+        
+    });
+
+        // When user deletes a data / item 
+    app.delete('/todo', (req, res) => {
+        
+    });
+}
